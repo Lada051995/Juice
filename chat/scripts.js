ad4cbeeddab77d4f78542bd0fc4a2dbcc59d6768
Lada051995 <mladda@mail.ru>
2015-03-15 20:16:09 +0200

    changed files

--- chat/scripts.js
+++ chat/scripts.js
@@ -1,15 +1,55 @@
+var messageOption = function (text, value){
+	return{
+		message : text,
+		id : value
+	};
+};
+
+var fullName = function (name, surname){
+	return{
+		name : name,
+		surname : surname
+	};
+};
+
+var listforSavingMessages = [];
+
 function run(){
 	var send = document.getElementById('send');
 	var rename = document.getElementById('rename');
 	var deletee = document.getElementById('delete');
 	var inSelect = document.getElementById('select');
 	var edit = document.getElementById('send_edit_message');
+	var onOff = document.getElementById('OnOff');
+	
+	if (restoreMessages() != null){
+		var allMessages = restoreMessages();
+		createAllMessages(allMessages);
+	}
+	
+	if (restoreName() != null){
+		var nameAndSurname = restoreName();
+		createNameSurname(nameAndSurname);
+	}
 	
 	send.addEventListener('click', EventSend);
 	rename.addEventListener('click', EventRename);
 	deletee.addEventListener('click', EventDelete);
 	inSelect.addEventListener('click', EventActionSelect);
 	edit.addEventListener('click', EventActionEdit);
+	onOff.addEventListener('click', EventOnOff);
+}
+
+function EventOnOff(evtObj){
+	var onoff = document.getElementById('OnOff');
+	if(onoff.className =='btn btn-danger'){
+		onoff.className = 'btn btn-success';
+		onoff.value = "ON";
+	}
+	else{
+	    onoff.className = 'btn btn-danger';
+	    onoff.value = "OFF";
+	}
 }
 
 function EventSend(evtObj){
@@ -22,6 +62,9 @@ function EventSend(evtObj){
 		option.value = select.length;
 		select.add(option);
 		areatext.value = "";
+		
+		listforSavingMessages.push(messageOption(option.text, option.value));
+		storeMessages(listforSavingMessages);
 		}
 }
 
@@ -30,14 +73,23 @@ function EventRename(evnObj){
 	var mySurname = document.getElementById('surname');
 	var textArea = document.getElementById('name_surname');
 	if(myName.value&&mySurname.value){
+		storeName(fullName(myName.value, mySurname.value));
 		textArea.value = myName.value + " " + mySurname.value;
+		myName.value = "";
+		mySurname.value = "";
+	   
 	}
 }
 
 function EventDelete(evnObj){
-	var select = document.getElementById('select');
+	var n_s = document.getElementById('name_surname');
+	var mySurname = document.getElementById('surname');
 	var edit_text = document.getElementById('textformessage');
-	select.remove(select.selectedIndex);// удаляет выбранный индекс
+	var index = document.getElementById('select').selectedIndex;
+	var select = document.getElementById('select')[index];
+	select.text = '\u2421';
+	listforSavingMessages[index] = messageOption(select.text, index);
+	storeMessages(listforSavingMessages);
 	edit_text.value = "";
 }
 
@@ -45,16 +97,80 @@ function EventActionSelect(evnObj){
 	var textarea = document.getElementById('textformessage');
 	var index = document.getElementById('select').selectedIndex;
 	var selected = document.getElementById('select')[index];
+	if(selected.text != '\u2421'){
 	var subindex = selected.text.indexOf(":");
-	textarea.value = selected.text.substring(subindex+1);
+	   if (selected.text.indexOf('\u270e')!=(-1)){
+	    	var subindex2 = selected.text.indexOf('\u270e');
+	    }
+	textarea.value = selected.text.substring(subindex+1, subindex2);
+	}
 }
 
 function EventActionEdit(evnObj){
-	var myName = document.getElementById('name');
+	var n_s = document.getElementById('name_surname');
 	var mySurname = document.getElementById('surname');
 	var edit_text = document.getElementById('textformessage');
 	var index = document.getElementById('select').selectedIndex;
 	var select = document.getElementById('select')[index];
-	select.text = myName.value+ " " + mySurname.value + ":" + edit_text.value;
+	if(select.text != '\u2421'){
+	select.text = n_s.value + ": "+ edit_text.value+ " " + '\u270e';
+	listforSavingMessages[index] = messageOption(select.text, index);
+	storeMessages(listforSavingMessages);
 	edit_text.value = "";
+	}
+}
+
+function storeMessages(listforSavingMessages){
+	if(typeof (Storage) == "undefined"){
+		alert('local storage is not accessible');
+		return;
+	}
+	localStorage.setItem("list messages", JSON.stringify(listforSavingMessages));
+}
+
+function restoreMessages(){
+	if(typeof (Storage) == "undefined"){
+		alert('local storage is not accessible');
+		return;
+	}
+	var item = localStorage.getItem("list messages");
+	return item && JSON.parse(item);
+}
+
+function createAllMessages(allMessages){
+	for (var i = 0; i < allMessages.length; i++){
+		listforSavingMessages.push(allMessages[i]);
+		addMessage(allMessages[i]);
+	}
+}
+
+function addMessage(message){
+	var selected = document.getElementById('select');
+	var option = document.createElement("option");
+	option.text = message.message;
+	option.value = message.id;
+	
+	selected.add(option);
+}
+
+function storeName(mystruct){
+	if (typeof (Storage) == "undefined"){
+		alert('local storage is not accessible');
+		return;
+	}
+	localStorage.setItem("MyName", JSON.stringify(mystruct));
+}
+
+function restoreName(){
+	if(typeof (Storage) == "undefined"){
+		alert('local storage in not accessible');
+		return;
+	}
+	var item = localStorage.getItem("MyName");
+	return item && JSON.parse(item);
+}
+
+function createNameSurname(nameAndSurname){
+	var textArea = document.getElementById('name_surname');
+	textArea.value = nameAndSurname.name + " " + nameAndSurname.surname;
 }
